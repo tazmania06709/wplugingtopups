cc03bdccbd70218de7df93cb4278a60e9afd4e74
JOSMAN VICENS NOA <josman.vicens@gmail.com>
2022-08-06 00:20:38 -0400

    funciones de activar, desaxctivar y borrar

--- wplugingtopups.php
+++ wplugingtopups.php
@@ -1,11 +1,22 @@
 <?php
 
 /*
-Plugin Name: Wplugingtopups
+Plugin Name: Wp Plugins TopUps
 Plugin URI: http://URI_Of_Page_Describing_Plugin_and_Updates
 Description: A brief description of the Plugin.
 Version: 1.0
-Author: JOSMAN VICENS NOA
+Author: Josman Vicéns Noa
 Author URI: http://URI_Of_The_Plugin_Author
 License: A "Slug" license name e.g. GPL2
 */
+
+function Activar(){
+
+}
+ function Desactivar(){
+
+ }
+
+ function Borrar(){
+
+ }

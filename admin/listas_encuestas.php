95ed8c0a634297a234561dc2ebe80765819dc2b3
JOSMAN VICENS NOA <josman.vicens@gmail.com>
2022-08-07 16:49:43 -0400

    admin

---
+++ admin/listas_encuestas.php
@@ -0,0 +1,26 @@
+<diV class="wrap"
+    <?php
+        echo "<h1>".get_admin_page_title()."</h1>";
+    ?>
+     <a class="page-title-action">Añadir nuevo:</a>
+     <br><br><br>
+   
+     <table class="wp-list-table widefat fixed striped pages">
+        <thead>
+            <th> Nombre de la encuesta</th>
+            <th>ShortCode</th>
+            <th>Acciones</th>
+        </thead>
+        <tbody id="the-list">
+            <tr>
+              <td>Primera encuesta</td>
+              <td>[CODE]</td>
+              <td>
+                <a class="page-title-action">Ver estadisticas</a>
+                <a class="page-title-action">Borrar</a>
+              </td>
+            </tr>  
+     </table>
+
+
+</div>   

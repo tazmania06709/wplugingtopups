2453c6a9cc250baf7fe01f4da251af2915e323e5
JOSMAN VICENS NOA <josman.vicens@gmail.com>
2022-08-14 09:56:35 -0400

    Remove pregunta

--- admin/js/lista_encuestas.js
+++ admin/js/lista_encuestas.js
@@ -12,4 +12,12 @@ jQuery(document).ready(function($){
         $("#camposdinamicos").append('<tr id="row'+i+'"><td><label for="txtnombre" class="col-form-label" style="margin-right:5px">Pregunta '+i+'</label></td><td><input type="text" name="name[]" id="name" class="form-control name_list"></td><td><button name="remove" id="'+i+'" class="btn btn-danger" style="margin-left:5px">X</button></td></tr>');
         return false;
     });
+    
+    $(document).on('click','.btn_remove',function(){
+        var button_id = $(this).attr('id');
+        $("#row"+button_id+"").remove();
+
+    });
+     
+
 }); 
